9c05a66d5200746013e34691f5bd2bd54a5c03b1
JoseLuisSeoaneAlen <84730113+JoseLuisSeoaneAlen@users.noreply.github.com>
2021-06-01 17:57:49 +0200

    Update ejemploRoutersJSON.py

--- ejemploRoutersJSON.py
+++ ejemploRoutersJSON.py
@@ -1,23 +1,20 @@
 # delaracion de 2 Routers
-{
-      “Routers”: [ 
-                   { 
-                     “Hostname”: “R1”,
-                     “Vendor”: “Cisco”, 
-                     “Id”: “1” 
-                   }, 
-                   { 
-                     “Hostname”: “R2”,
-                     “Vendor”: “Huawei”,
-                     “Id”: “2” 
+import json
+jsonvar = json.dumps({
+      "Routers": [
+                   {
+                     "Hostname": "R1",
+                     "Vendor": "Cisco",
+                     "Id": "1"
+                   },
+                   {
+                     "Hostname": "R2",
+                     "Vendor": "Huawei",
+                     "Id": "2"
                    }
                 ]
-}
-
-# guardamos los routers declarados en json_routers.txt
-import json
- 
-with open('json_routers.txt') as json_data:
-    json_parsed = json.loads(json_routers.read())
- 
-print(json.dumps(json_parsed, indent=4, sort_keys=True))
+}, indent=4, sort_keys=True)
+fo = open("fichero.txt", "w")
+fo.write(jsonvar)
+# Cerramos el archivo fichero.txt
+fo.close()
